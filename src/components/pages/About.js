f2772be9605db6c0eaf1f038cc9eebe92f11676f
Nolan Frame <frame.nolan@gmail.com>
2022-05-31 12:11:53 -0700

    basic page layout and styling

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../style/About.css'
+
+function About() {
+
+    return (
+        <section className="About">
+            <p>This is our About Us page</p>
+        </section>
+    )
+}
+
+export default About;
