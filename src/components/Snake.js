cdb7dd4ec78bd08ac35a01f6ac9be6a5391ec46b
minhkhoinguy <minhkhoinguy@gmail.com>
2022-06-03 11:52:49 -0700

    basicfile4Snakegame

---
+++ src/components/Snake.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/Snake.css'
+
+export default function Snake() {
+  return (
+    <div>
+        <h1>This is Snake Game</h1>
+        <button>Start</button>
+    </div>
+  )
+}
