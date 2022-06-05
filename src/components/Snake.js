5161c22d67c3fccb7b0a2a0410987d065b7b5441
minhkhoinguy <minhkhoinguy@gmail.com>
2022-06-04 21:30:27 -0700

    snakeGameLogic

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import '../style/Snake.css'
+import React, { Component } from "react";
+import "../style/Snake.css";
+import Board from "./Board";
 
-export default function Snake() {
-  return (
-    <div>
-        <h1>This is Snake Game</h1>
-        <button>Start</button>
-    </div>
-  )
+class Snake extends Component {
+  render() {
+    return (
+      <div className="Snake">
+        <div className="Snake-header">
+          <h1>Snake</h1>
+          <p>
+            Click the button to start. Use the arrow keys to control the
+            direction of the snake.
+          </p>
+        </div>
+        <Board />
+      </div>
+    );
+  }
 }
+export default Snake;
