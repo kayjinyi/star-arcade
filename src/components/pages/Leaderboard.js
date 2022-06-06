352e965b4b23e3523b593458b94520924eed1204
ester kim <kimester1996@gmail.com>
2022-06-06 13:20:22 -0800

    testing seeds now

--- src/components/pages/Leaderboard.js
+++ src/components/pages/Leaderboard.js
@@ -4,11 +4,16 @@ import { api } from "../../util/api";
 import ScorePost from "../ScorePost";
 
 function Leaderboard() {
-  const [scores, setScores] = useState({});
+  const [scores, setScores] = useState([]);
   const getScores = async () => {
     try {
       const res = await api.getScores();
       const scorePosts = await res.json();
+      const newScore = [ ...scorePosts];
+    console.log(newScore);
+
+    // Call setBucket to update state with our new set of bucket list items
+    setScores(newScore);
     //   setScores(scorePosts);
     } catch (err) {
       console.error(err);
@@ -23,11 +28,8 @@ function Leaderboard() {
 //     score = '',
    
 //   } = scores;
-  const newScore = [ ...scorePosts];
-    console.log(newScore);
 
-    // Call setBucket to update state with our new set of bucket list items
-    setScores(newScore);
+  
 
   return (
     <section className="Leaderboard">
