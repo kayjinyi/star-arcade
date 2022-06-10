20cb0657a8473703a5343530c9c9fed0b57d7102
Ester Kim <99621150+kimester@users.noreply.github.com>
2022-06-10 14:20:39 -0800

    Update api.js

--- src/util/api.js
+++ src/util/api.js
@@ -1,35 +1,35 @@
-// export const api = {
-//     getScores: async function () {
-//       return fetch("https://koolgamebackend.herokuapp.com/userscore");
-//       // return await res.json();
+export const api = {
+    getScores: async function () {
+      return fetch("https://koolgamebackend.herokuapp.com/userscore");
+      // return await res.json();
      
 
-//     },
-//     createUser: async function (user) {
-//       return fetch("https://koolgamebackend.herokuapp.com/userscore", {
-//         method: "POST",
-//         body: JSON.stringify(user),
-//         headers: {
-//           "Content-type": "application/json; charset=UTF-8",
-//         },
-//       });
-//     },
-//   };
+    },
+    createUser: async function (user) {
+      return fetch("https://koolgamebackend.herokuapp.com/userscore", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+    },
+  };
 
-export const api = {
-  getScores: async function () {
-    return fetch("http://localhost:3001/api/userscore");
-    // return await res.json();
+// export const api = {
+//   getScores: async function () {
+//     return fetch("http://localhost:3001/api/userscore");
+//     // return await res.json();
    
 
-  },
-  createUser: async function (user) {
-    return fetch("http://localhost:3001/api/userscore", {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
-  },
-};
+//   },
+//   createUser: async function (user) {
+//     return fetch("http://localhost:3001/api/userscore", {
+//       method: "POST",
+//       body: JSON.stringify(user),
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//       },
+//     });
+//   },
+// };
